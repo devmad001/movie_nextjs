daaa16823c034399db3f8f8d0e5a82f52b0e905c
devmad001 <shinnysmile0901@gmail.com>
2024-01-01 16:20:09 +0545

    fix : conflict between system and default theme set by user

--- src/components/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {MdLightMode} from "react-icons/md";
 import {BsFillMoonFill} from "react-icons/bs";
 import {useTheme} from "next-themes";
@@ -9,10 +9,15 @@ import {useTheme} from "next-themes";
 const DarkModeSwitch = () => {
     const {systemTheme, theme, setTheme} = useTheme();
     const currentTheme = theme === "system" ? systemTheme : theme;
+    const [mounted, setMounted] = useState(false);
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
     return (
 
         <>
-            {currentTheme === "dark" ? (<MdLightMode className='text-xl cursor-pointer hover:text-amber-500'
+            {mounted && currentTheme === "dark" ? (<MdLightMode className='text-xl cursor-pointer hover:text-amber-500'
                                                      onClick={() => setTheme("light")}/>) : (
                 <BsFillMoonFill className='text-xl cursor-pointer hover:text-amber-500'
                                 onClick={() => setTheme("dark")}/>)}
