d244da5dc6c0143b4bcc28b08e169798333152e5
devmad001 <shinnysmile0901@gmail.com>
2024-01-01 12:12:06 +0545

    Link to home

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Link from "next/link";
 
 const MenuItem = ({title, address, Icon}) => {
     return (
         <div>
-            
+            <Link href={address}>
+                <a>
+                    <p>{title}</p>
+                </a>
+            </Link>
         </div>
     );
 };
