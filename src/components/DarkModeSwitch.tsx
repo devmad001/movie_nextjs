c3521a2eaddd6b60567f47ac77743101fe219171
devmad001 <shinnysmile0901@gmail.com>
2024-01-01 16:12:08 +0545

    set conditional operation for themes

--- src/components/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch.tsx
@@ -5,12 +5,16 @@ import React from 'react';
 import {MdLightMode} from "react-icons/md";
 import {BsFillMoonFill} from "react-icons/bs";
 import {useTheme} from "next-themes";
+
 const DarkModeSwitch = () => {
     const {systemTheme, theme, setTheme} = useTheme();
+    const currentTheme = theme === "system" ? systemTheme : theme;
     return (
+
         <>
-            <MdLightMode/>
-            <BsFillMoonFill/>
+            {currentTheme === "dark" ? (<MdLightMode/>) : (<BsFillMoonFill/>)}
+
+
         </>
     );
 };
