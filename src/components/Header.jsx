3f28f17726fe3b4c6327d0cf831c9fb0a5731ad4
devmad001 <shinnysmile0901@gmail.com>
2024-01-01 12:22:54 +0545

    add about in menuitem

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import MenuItem from "@/components/MenuItem";
 import {AiFillHome} from "react-icons/ai";
+import {BsFillInfoCircleFill} from "react-icons/bs";
 
 const Header = () => {
     return (<div>
         <div className=''>
 
             <MenuItem title='HOME' address='/' Icon={AiFillHome}/>
+            <MenuItem title='ABOUT' address='/about' Icon={BsFillInfoCircleFill}/>
         </div>
         <div className=''>
 
