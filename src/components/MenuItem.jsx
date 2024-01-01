00347cbcdb70c03171a6e8a376398ccbde72db77
devmad001 <shinnysmile0901@gmail.com>
2024-01-01 12:19:39 +0545

    more style to p tag to hide in small size

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -6,7 +6,7 @@ const MenuItem = ({title, address, Icon}) => {
         <div>
             <Link href={address} className='mx-4 lg:mx-6 hover:text-amber-600'>
                 <Icon className='text-2xl sm:hidden'/>
-                <p>{title}</p>
+                <p className='hidden sm:inline my-2 text-sm '>{title}</p>
             </Link>
         </div>
     );
