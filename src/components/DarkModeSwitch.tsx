d16cd1e8367e4c139485dd7a238b626065a6984d
devmad001 <shinnysmile0901@gmail.com>
2024-01-01 16:08:44 +0545

    initialise useTheme

--- src/components/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch.tsx
@@ -1,8 +1,12 @@
+'use client'
+
+
 import React from 'react';
 import {MdLightMode} from "react-icons/md";
 import {BsFillMoonFill} from "react-icons/bs";
-
+import {useTheme} from "next-themes";
 const DarkModeSwitch = () => {
+    const {systemTheme, theme, setTheme} = useTheme();
     return (
         <>
             <MdLightMode/>
