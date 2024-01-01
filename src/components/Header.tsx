36eaa7e8dddfdadf3a0732d05c3e03be13b2b33c
devmad001 <shinnysmile0901@gmail.com>
2024-01-01 14:12:37 +0545

    style : add logo by styling moviehub

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,26 @@ import React from 'react';
 import MenuItem from "@/components/MenuItem";
 import {AiFillHome} from "react-icons/ai";
 import {BsFillInfoCircleFill} from "react-icons/bs";
+import Link from "next/link";
 
 const Header = () => {
     return (<div>
-        <div className=''>
+        <div className='flex'>
 
             <MenuItem title='HOME' address='/' Icon={AiFillHome}/>
             <MenuItem title='ABOUT' address='/about' Icon={BsFillInfoCircleFill}/>
         </div>
         <div className=''>
+            <Link href='/'>
+                <h2 className='text-2xl'>
+                    <span className='font-bold bg-amber-500 py-1 px-2 rounded-full '>
+                        MovieHub
+
+                    </span>
+
+                </h2>
+
+            </Link>
 
         </div>
 
