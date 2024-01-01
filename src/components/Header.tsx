14ac9c0623ec00d3b1e380632f05f546343d1949
devmad001 <shinnysmile0901@gmail.com>
2024-01-01 14:21:38 +0545

    complete Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,26 +5,20 @@ import {BsFillInfoCircleFill} from "react-icons/bs";
 import Link from "next/link";
 
 const Header = () => {
-    return (<div>
-        <div className='flex'>
-
-            <MenuItem title='HOME' address='/' Icon={AiFillHome}/>
-            <MenuItem title='ABOUT' address='/about' Icon={BsFillInfoCircleFill}/>
+    return (<div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
+        <div className="flex">
+            <MenuItem title="HOME" address="/" Icon={AiFillHome}/>
+            <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill}/>
         </div>
-        <div className=''>
-            <Link href='/'>
-                <h2 className='text-2xl'>
-                    <span className='font-bold bg-amber-500 py-1 px-2 rounded-full '>
-                        MovieHub
-
-                    </span>
-
+        <div className="flex items-center space-x-5">
+            <Link href="/">
+                <h2 className="text-2xl">
+            <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">
+              MovieHub
+            </span>
                 </h2>
-
             </Link>
-
         </div>
-
     </div>);
 };
 
