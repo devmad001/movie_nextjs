63d02552ffba5f467e50a331006f133ae0fa9d89
devmad001 <shinnysmile0901@gmail.com>
2024-01-01 16:25:05 +0545

    style : dark and light themes to gray

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -7,8 +7,12 @@ import {ThemeProvider} from "next-themes";
 // @ts-ignore
 const Providers = ({children}) => {
     return (
-        <ThemeProvider enableSystem={true} attribute="class" >
-            {children}
+        <ThemeProvider enableSystem={true} attribute="class">
+            <div
+                className='dark:bg-gray-700 dark:text-gray-200 text-gray-700 transition-colors duration-300 min-h-screen '>
+
+                {children}
+            </div>
         </ThemeProvider>
     );
 };
