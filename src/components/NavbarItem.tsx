3c08d3ae30f5a1caa2435fb2ba43d4ac91d683c8
devmad001 <shinnysmile0901@gmail.com>
2024-01-02 18:46:28 +0545

    complete : NavbarItem.tsx

--- src/components/NavbarItem.tsx
+++ src/components/NavbarItem.tsx
@@ -1,11 +1,18 @@
+'use client';
+
 import React from 'react';
 import Link from "next/link";
+import {useSearchParams} from "next/navigation";
 
 // @ts-ignore
 const NavbarItem = ({title, param}) => {
+    const searchParams = useSearchParams();
+    const genre = searchParams.get('genre');
     return (
         <div>
-            <Link href={`/?genre=${param}`}>
+            <Link
+                className={`m-4 hover:text-amber-600 font-semibold p-2 ${genre && genre === param && "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"} `}
+                href={`/?genre=${param}`}>
                 {title}
             </Link>
         </div>
