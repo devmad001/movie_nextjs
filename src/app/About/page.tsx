0c50afbcb62dbe19ca1502fd7615dba817244ff5
devmad001 <shinnysmile0901@gmail.com>
2024-01-02 17:23:44 +0545

    style : made from x to y axis

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Page = () => {
     return (
-        <div className='max-w-6xl mx-auto space-x-4'>
+        <div className='max-w-6xl mx-auto space-y-4'>
             <h1 className='text-2xl font-medium text-amber-600'>About</h1>
             <h1></h1>
         </div>
