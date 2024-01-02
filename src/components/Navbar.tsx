d59052aa11f427b81a92442072788192be8797dc
devmad001 <shinnysmile0901@gmail.com>
2024-01-02 17:57:47 +0545

    add navbaritem params

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div>
-            
+            <NavbarItem title='Trending' param='fetchTrending'/>
+            <NavbarItem title='Top Rated' param='fetchTopRated'/>
         </div>
     );
 };
