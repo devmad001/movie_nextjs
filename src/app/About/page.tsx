f5f80fb4492da158e6d9d2194b778b4037f15750
devmad001 <shinnysmile0901@gmail.com>
2024-01-02 17:21:44 +0545

    initialise about page

---
+++ src/app/About/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div className='max-w-6xl mx-auto space-x-4'>
+            <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+            <h1></h1>
+        </div>
+    );
+};
+
+export default Page;
