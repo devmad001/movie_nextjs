3c92881504b8e33a146e400a318d8d7901c5c851
devmad001 <shinnysmile0901@gmail.com>
2024-01-02 19:00:59 +0545

    fetch : data from tmdb api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 
-export default function Home() {
+
+
+const API_KEY = process.env.API_KEY;
+// @ts-ignore
+export default async function Home({searchParams}) {
+    const genre = searchParams.genre || 'fetchTrending';
+    const res = await fetch(`https://api.themoviedb.org/3/movie/${genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"}?api_key=${API_KEY}&language=en-US&page=1`);
+
+
 
     return (
+
+
         <h1 className='text-4xl'>Home</h1>
     )
 }
