65af52f759e5ff02cd7df1c59bec57442117ecb1
devmad001 <shinnysmile0901@gmail.com>
2024-01-02 19:09:28 +0545

    typo : error in fetching url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 const API_KEY = process.env.API_KEY;
+
 // @ts-ignore
 export default async function Home({searchParams}) {
     const genre = searchParams.genre || 'fetchTrending';
-    const res = await fetch(`https://api.themoviedb.org/3/movie/${genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"}?api_key=${API_KEY}&language=en-US&page=1`, {next: {revalidate: 10000}});
+    const endpoint = genre === 'fetchTopRated' ? 'movie/top_rated' : 'trending/all/week';
+    const res = await fetch(`https://api.themoviedb.org/3/${endpoint}?api_key=${API_KEY}&language=en-US&page=1`, {next: {revalidate: 9000}});
+    const data = await res.json();
 
+    const results = data.results;
+    console.log(results);
 
     return (
-
-
         <h1 className='text-4xl'>Home</h1>
-    )
+    );
 }
