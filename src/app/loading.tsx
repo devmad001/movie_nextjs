96be4dffe4f3e2ff0a16a8bd0f09bb09006075d6
devmad001 <shinnysmile0901@gmail.com>
2024-01-02 22:57:58 +0545

    add : loading effect

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+export default function Loading() {
+    // Or a custom loading skeleton component
+    return (
+
+
+        <div className='flex justify-center'>
+            <img className='h-96' src='spinner.svg' alt='Loading...'/>
+        </div>
+    )
+}
