c3f3abed4265dccabaea450f3059edfd730c8686
devmad001 <shinnysmile0901@gmail.com>
2024-01-03 08:27:48 +0545

    add : image in card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import Link from "next/link";
+import Image from "next/image";
 
 // @ts-ignore
 const Card = ({result}) => {
     return (
         <div
             className='cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200'>
-            {result.id}
+            <Link href={`/movie/${result.id}`}>
+                <Image
+                    src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`} width={500} height={300}>
+
+                </Image>
+
+
+            </Link>
         </div>
     );
 };
