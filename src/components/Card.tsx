78ec07e2c80309990c716783714ba16a840559ca
devmad001 <shinnysmile0901@gmail.com>
2024-01-03 08:23:26 +0545

    style : Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 // @ts-ignore
 const Card = ({result}) => {
     return (
-        <div>
+        <div
+            className='cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200'>
             {result.id}
         </div>
     );
