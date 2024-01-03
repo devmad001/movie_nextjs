43030300150a808cfba70d47e7122a31337d7759
devmad001 <shinnysmile0901@gmail.com>
2024-01-03 19:24:52 +0545

    movie datas fetched by id

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,3 +1,13 @@
-export default function Page() {
-    return <div>Page</div>;
+
+
+
+// @ts-ignore
+
+export default async function MoviePage({params}) {
+    const movieId = params.id;
+    const res = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}&language=en-US`)
+    const movie = await res.json();
+    return <div>
+        <h1>{movie.title || movie.name}</h1>
+    </div>;
 }
