66b4a9a058d481bf21cebb4cf979fe5f631f3a8e
devmad001 <shinnysmile0901@gmail.com>
2024-01-04 17:41:51 +0545

    chore : fetch image in movie/[id] route

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,13 +1,32 @@
-
-
-
 // @ts-ignore
 
+import Image from "next/image";
+import React from "react";
+
+// @ts-ignore
 export default async function MoviePage({params}) {
     const movieId = params.id;
     const res = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}&language=en-US`)
     const movie = await res.json();
-    return <div>
-        <h1>{movie.title || movie.name}</h1>
+    return <div className='w-full'>
+
+        <div
+            className='py-4 md:pt-8 flex flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6'>
+            <Image alt="Image"
+                   src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path || movie.poster_path}`}
+                   width={500} height={300}
+                   style={
+                       {
+                           maxWidth: '100%',
+                           height: '100%',
+                       }
+                   }
+                   className='rounded-lg'
+                   placeholder='blur'
+                   blurDataURL='/spinner.svg'>
+
+            </Image>
+
+        </div>
     </div>;
 }
