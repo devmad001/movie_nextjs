9405c16ac70c0c568c8c2fba3058087fa698a6f8
devmad001 <shinnysmile0901@gmail.com>
2024-01-04 17:51:07 +0545

    complete : movie page

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -26,6 +26,16 @@ export default async function MoviePage({params}) {
                    blurDataURL='/spinner.svg'>
 
             </Image>
+            <div className='p-2'>
+                <h2 className='text-lg mb-3 font-bold'>{movie.title || movie.name}</h2>
+
+                <p className='text-lg mb-3'><span className='font-semibold mr-1'>Overview :</span>{movie.overview}</p>
+                <p className='mb-3'><span
+                    className='font-semibold mr-1'>Date Released :</span>{movie.release_date || movie.first_air_date}
+                </p> <p className='mb-3'><span
+                className='font-semibold mr-1'>Rating :</span>{movie.vote_count}</p>
+
+            </div>
 
         </div>
     </div>;
